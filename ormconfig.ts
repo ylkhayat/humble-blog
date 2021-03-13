fe51cfe4fb20d4b385bd40be56065de55a6b552a
Youssef Tarek <yousseftarek@live.com>
2021-03-13 02:11:24 +0200

    docker running config

---
+++ ormconfig.ts
@@ -0,0 +1,12 @@
+const typeOrmConfig = {
+  type: 'mysql',
+  host: process.env.MYSQL_HOST,
+  port: Number.parseInt(process.env.MYSQL_PORT, 10) || '',
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  entities: ['dist/**/*.entity.js'],
+  synchronize: true,
+};
+
+module.exports = typeOrmConfig;
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as env from 'dotenv';
 
 declare const module: any;
+
+env.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
